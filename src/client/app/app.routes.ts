937790bda91228fc29b2c99b627e671943398fda
Nik Nauwelaerts <Nik Nauwelaerts>
2016-10-24 17:12:41 +0200

    first commit

---
+++ src/client/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+import { LoginRoutes } from './login/index';
+import { SignupRoutes } from './signup/index';
+import { DashboardRoutes } from './dashboard/index';
+
+export const routes: Routes = [
+	...LoginRoutes,
+	...SignupRoutes,
+	...DashboardRoutes
+];
